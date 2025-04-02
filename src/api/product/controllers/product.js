91281cd0ba1476564a5f51183d41671cc942220c
Maks Charyev <helloimaksim@gmail.com>
2025-04-02 03:06:36 +0300

    recommend to product

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -6,4 +6,26 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController('api::product.product', ({ strapi }) => ({
+  async toggleRecommendation(ctx) {
+    const { id } = ctx.params; // ID товара из запроса
+
+    // Получаем текущий товар
+    const product = await strapi.entityService.findOne('api::product.product', id, {
+      fields: ['isRecommended'],
+    });
+
+    if (!product) {
+      return ctx.notFound('Товар не найден');
+    }
+
+    // Инвертируем значение isRecommended
+    const updatedProduct = await strapi.entityService.update('api::product.product', id, {
+      data: {
+        isRecommended: !product.isRecommended,
+      },
+    });
+
+    return ctx.send({ product: updatedProduct });
+  },
+}));
